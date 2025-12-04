7f533a2608212a572ca2e07b3986e2db2706a2af
lavanyasatagopam <137160605+satagop@users.noreply.github.com>
2025-12-04 16:21:00 +0530

    Insert sample data into students table --code program query

---
+++ data.sql
@@ -0,0 +1,6 @@
+-- Insert Sample Data
+INSERT INTO students (student_id, name, branch, marks, email) VALUES
+(1, 'Lavanya', 'CSE', 88, 'lavanya@example.com'),
+(2, 'Ravi', 'ECE', 75, 'ravi@example.com'),
+(3, 'Priya', 'IT', 92, 'priya@example.com'),
+(4, 'Kiran', 'EEE', 67, 'kiran@example.com');
